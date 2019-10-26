8f9e8ab75bac3bef9bdf1aedd350a45849e26610
Jariza28 <jariz@anativapps.co>
2019-10-26 18:43:24 -0500

    Antes de ngModal

--- src/app/admin/project/project.component.ts
+++ src/app/admin/project/project.component.ts
@@ -16,10 +16,30 @@ export class ProjectComponent implements OnInit {
         icon: 'fa fa-user fa-lg'
       },
       {
+        name: 'Planes o combos',
+        link: 'create',
+        icon: 'fa fa-dashboard fa-lg'
+      },
+      {
+        name: 'Servicios individuales',
+        link: 'create',
+        icon: 'fa fa-dashboard fa-lg'
+      },
+      {
+        name: 'Ventas',
+        link: 'create',
+        icon: 'fa fa-dashboard fa-lg'
+      },
+      {
         name: 'Zonas',
         link: 'create',
         icon: 'fa fa-dashboard fa-lg'
-      }
+      },
+      {
+        name: 'Ajencias',
+        link: 'create',
+        icon: 'fa fa-dashboard fa-lg'
+      },
     ];
   }
 
--- src/app/auth/auth.service.service.ts
+++ src/app/auth/auth.service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { auth } from 'firebase/app';
-import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFireAuth } from '@angular/fire/auth';
 import { User } from 'firebase';
 
 @Injectable({
@@ -25,7 +25,7 @@ export class AuthService {
    */
   async  login(email: string, password: string) {
     try {
-      await this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      await this.afAuth.auth.signInWithEmailAndPassword(email, password);
       this.router.navigate(['admin/list']);
     } catch (e) {
       alert('Error!' + e.message);
