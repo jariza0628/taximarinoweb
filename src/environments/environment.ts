1f1b86f0d3217f57c8205fd0efe00bce7d6f70ca
Jariza28 <jariz@anativapps.co>
2020-09-14 16:26:32 -0500

    cambios estilos ventas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
+ /* firebaseConfig: {
     apiKey: 'AIzaSyAopyqbc9NmT4rAuQ6_FsJse11FTRzOlUA',
     authDomain: 'dev-taximarino.firebaseapp.com',
     databaseURL: 'https://dev-taximarino.firebaseio.com',
@@ -13,8 +13,20 @@ export const environment = {
     messagingSenderId: '352440104332',
     appId: '1:352440104332:web:6eae0ec6468da0acb252a9',
     measurementId: 'G-QHZ72YFG6C'
+  }*/
+  //PROD
+  firebaseConfig: {
+    apiKey: 'AIzaSyDhp-1ADAPXE6ypuMMGjHHw_PP-ndWoQ8Q',
+    authDomain: 'taximarino-98c35.firebaseapp.com',
+    databaseURL: 'https://taximarino-98c35.firebaseio.com',
+    projectId: 'taximarino-98c35',
+    storageBucket: 'taximarino-98c35.appspot.com',
+    messagingSenderId: '4435333264',
+    appId: '1:4435333264:web:c1cac4d754eabb21564815'
   }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
